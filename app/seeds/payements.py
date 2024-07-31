bb139756975f33ffe04f8cf188a36c59ee8e01ee
Anurag Reddy <122401303+anureddy11@users.noreply.github.com>
2024-07-30 20:53:58 -0700

    added all seeders and migrations

---
+++ app/seeds/payements.py
@@ -0,0 +1,25 @@
+from app.models import db, User, environment, SCHEMA, Payment, Expense, ExpenseShare, Friend, Comment
+from sqlalchemy.sql import text
+
+def seed_payments():
+    payment1 = Payment(
+        payer_id=1, payee_id=2, status='completed', amount=100.00, comment_id=1)
+    payment2 = Payment(
+        payer_id=2, payee_id=3, status='pending', amount=150.00, comment_id=2)
+    payment3 = Payment(
+        payer_id=3, payee_id=1, status='failed', amount=200.00, comment_id=3)
+
+    db.session.add(payment1)
+    db.session.add(payment2)
+    db.session.add(payment3)
+    db.session.commit()
+
+
+
+def undo_payments():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.payments RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM payments"))
+
+    db.session.commit()

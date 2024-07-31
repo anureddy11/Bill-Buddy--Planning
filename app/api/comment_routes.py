4c174431cd1304e3de0c3ae8dd061c4ff3f79b6e
Derek Lin <12linderek@gmail.com>
2024-07-30 20:17:07 -0700

    created comment_routes.py file and registered blueprint

---
+++ app/api/comment_routes.py
@@ -0,0 +1,9 @@
+from app.models import User, db, Comment
+from flask_login import current_user, login_required
+from flask import Blueprint, request, jsonify
+from datetime import datetime
+
+comment_routes = Blueprint('comment', __name__)
+
+# Create a comment
+@comment_routes.route('/api/')

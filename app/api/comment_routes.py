693e15a4b9ed1ba70ee458ab623ae4556e477044
Derek Lin <12linderek@gmail.com>
2024-07-31 20:43:39 -0700

    added foreign key for expense_id in comments table and finish create comments route

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -1,10 +1,39 @@
 from app.models import User, db, Comment
 from flask_login import current_user, login_required
 from flask import Blueprint, request, jsonify
-from datetime import datetime
+from datetime import datetime, timezone
 
 comment_routes = Blueprint('comment', __name__)
 
 # Create a comment
-@comment_routes.route('/api/')
+@comment_routes.route('/<int:expenseId>/comments', methods=['POST'])
 @login_required
+def create_comment(expenseId):
+    """
+    Creates a new comment for the specified expense by expenseId
+    """
+    # Extract the JSON data
+    data = request.get_json()
+    content = data.get('content')
+    
+    
+    # Error validation if no content is present with 400 status code
+    if not content:
+        return jsonify({
+            "message": "Validation error",
+            "errors": {"content": "Content is required"}
+        }), 400
+    
+    # Create new instance of comment
+    new_comment = Comment(
+        user_id=current_user.id,
+        expense_id=expenseId,
+        content=content,
+        created_at=datetime.now(timezone.utc),
+        updated_at=datetime.now(timezone.utc)        
+    )
+    
+    db.session.add(new_comment)
+    db.session.commit()
+    
+    return jsonify(new_comment.to_dict()), 201
--- app/models/comment.py
+++ app/models/comment.py
@@ -1,18 +1,22 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from datetime import datetime
+from datetime import datetime, timezone
 
 
 class Comment(db.Model):
     __tablename__ = "comments"
+    
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
 
     id = db.Column(db.Integer, primary_key = True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    expense_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    expense_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('expenses.id')), nullable=False)
     content = db.Column(db.String(500), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc), nullable=False)
+    updated_at = db.Column(db.DateTime, default=datetime.now(timezone.utc), onupdate=datetime.datetime.now(timezone.utc), nullable=False)
 
     #Relationship for user
     user = db.relationship('User', back_populates='comments')
